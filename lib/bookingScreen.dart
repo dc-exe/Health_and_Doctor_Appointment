55eed5f8af9efcf1cf88de533cbfe12eaefc08d1
Darshak <darshakchavda2000@gmail.com>
2021-03-09 20:51:12 +0530

    'commit'

--- lib/bookingScreen.dart
+++ lib/bookingScreen.dart
@@ -1,10 +1,7 @@
-import 'dart:developer';
-
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:cool_alert/cool_alert.dart';
 import 'package:health_and_doctor_appointment/myAppointments.dart';
 import 'package:intl/intl.dart';
 
